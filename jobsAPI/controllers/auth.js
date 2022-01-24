78a22030a9b75e79944155e80f3cc3853675c7ba
Adedeji <Ayodeji.Adedeji@stanbicibtc.com>
2022-01-24 09:17:03 +0100

    login endpoint

--- jobsAPI/controllers/auth.js
+++ jobsAPI/controllers/auth.js
@@ -1,37 +1,49 @@
-const saltRounds = 10;
-const bcrypt = require('bcrypt');
+require('dotenv').config()
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 const { StatusCodes } = require('http-status-codes')
 const { customError } = require('../middlewares/customError')
 
 const register = async (req, res, next) => {
     try {
-        const { password } = req.body
-
-        bcrypt.hash(password, saltRounds, async function(err, hash) {
-            if(err){
-                return next(new customError(err, StatusCodes.BAD_REQUEST))
-            }
-            req.body.password = hash
-            
-            try {
-                const user = await User.create(req.body)
-                return res.status(StatusCodes.CREATED).json(user)
-            } 
-            catch (error) {
-                if(error.code = 11000){
-                    return next(new customError(`This email address is not available.`, StatusCodes.BAD_REQUEST))
-                }
-            }
-        }); 
-    } 
+        const user = await User.create(req.body)
+        const { name, emailAddress } = user
+        return res.status(StatusCodes.CREATED).json({
+            name:name, 
+            emailAddress:emailAddress
+        })
+    }
     catch (error) {
+        if(error.code == 11000){
+            return next(new customError(`This email address is not available.`, StatusCodes.BAD_REQUEST))
+        }
         return next(new customError(error, StatusCodes.BAD_REQUEST))
+        
     }
 }
 
-const login = (req, res, next) => {
-    res.status(200).json({message:"live"})
+const login = async (req, res, next) => {
+    const { name, password } = req.body
+
+    if(!name || name == '' || !password || password == ''){
+        return next(new customError(`Please provide your username and password.`, StatusCodes.BAD_REQUEST))
+    }
+
+    const userWithName = await User.findOne({name})
+
+    if(userWithName === null){
+        return next(new customError(`Invalid credentials.`, StatusCodes.UNAUTHORIZED))
+    }
+
+    const checkIfPasswordIsCorrect = bcrypt.compareSync(password, userWithName.password)
+
+    if(!checkIfPasswordIsCorrect){
+        return next(new customError(`Invalid credentials.`, StatusCodes.UNAUTHORIZED))
+    }
+
+    const token = jwt.sign({id:userWithName.id}, process.env.SECRET_KEY, {expiresIn:'1h'})
+    return res.status(200).json({id: userWithName.name, token : token})
 }
 
 module.exports = {
--- jobsAPI/models/user.js
+++ jobsAPI/models/user.js
@@ -1,8 +1,12 @@
+const bcrypt = require('bcryptjs')
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
     name : {
         type: String,
+        unique: true,
+        minlength: [6, 'Username must be more than 6 characters.'],
+        maxlength: [15, 'Username must be less than 15 characters.'],
         required: [true, "Name is required."]
     },
     emailAddress : {
@@ -17,5 +21,10 @@ const userSchema = mongoose.Schema({
     }
 })
 
+userSchema.pre('save', async function (next){
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
 
 module.exports = mongoose.model('User', userSchema)
