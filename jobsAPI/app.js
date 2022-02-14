88aef8692b945ea5380461ca3a4dc55d0c03ba7a
Adedeji <Ayodeji.Adedeji@stanbicibtc.com>
2022-02-14 17:26:13 +0100

    Refined the errors

--- jobsAPI/app.js
+++ jobsAPI/app.js
@@ -1,13 +1,31 @@
 require('dotenv').config()
+require('express-async-errors')
 const express = require('express')
 const app = express()
 
-require('express-async-errors')
+
+const cors = require('cors')
+const helmet = require('helmet')
+const xss = require('xss-clean')
 const connect = require('./db/connect')
 const authRoutes = require('./routes/auth')
 const jobsRoutes = require('./routes/jobs')
+const rateLimit = require('express-rate-limit')
 const verifyToken = require('./middlewares/auth')
 const errorHandler = require('./middlewares/errorHandler')
+
+//SECURITY PACKAGES
+app.set('trust proxy', 1)
+app.use(
+    rateLimit({
+        windowsMs: 2 * 60 * 1000,   //2 MINUTES
+        max: 10                     //10 REQUESTS PER WINDOWMS FOR AN IP
+    })
+)
+app.use(xss())
+app.use(cors())
+app.use(helmet())
+
 
 //MIDDLEWARES & ROUTES
 app.use(express.json())
